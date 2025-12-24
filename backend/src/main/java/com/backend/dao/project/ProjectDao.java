2648576fa94ca2213e8b87e17a804273ca0c3842
ayoub <mahdaoui.ayoub35@gmail.com>
2025-12-24 10:17:33 +0100

    security done

--- backend/src/main/java/com/backend/dao/project/ProjectDao.java
+++ backend/src/main/java/com/backend/dao/project/ProjectDao.java
@@ -1,4 +1,22 @@
 package com.backend.dao.project;
 
-public class ProjectDao {
+import com.backend.bean.project.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface ProjectDao extends JpaRepository<Project,Long> {
+
+    @Query("SELECT p FROM Project p JOIN p.teams t WHERE p.name = :name AND t.id = :teamId ORDER BY p.createdDate DESC")
+    List<Project> findByNameAndTeamIdOrderByCreatedDate(String name,Long teamId);
+    @Query("SELECT p FROM Project p JOIN p.teams t WHERE t.id = :teamId ORDER BY p.createdDate DESC")
+    List<Project> findAllByTeamIdOrderByCreatedDate(Long teamId);
+    @Modifying
+    @Query("DELETE FROM Project p WHERE p.name = :name AND :teamId IN (SELECT t.id FROM p.teams t)")
+    void deleteByNameAndTeamId(String name,Long teamId);
 }
